764d86394bf90fb4058de1eb624ffdbe97adedd3
saturnhafen <saturnhafen@gmail.com>
2022-03-08 14:17:51 +0100

    format flagfinder to single file

--- flagfinder.sh
+++ flagfinder.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 # connect via ssh
-if [[ $1 ]]; then
-  ssh $1 "
-
-echo 'Searching for <flags/interesting_file> with content <isec{.*}>';
-find / -type f -name flags/interesting_file -exec grep 'isec{.*}'  {} \;
-
-echo 'Searching for file with content <isec{.*}>';
-find -type f -exec grep 'isec{.*}'  {} \;
-
-echo 'Searching environment of current process';
-printenv | grep 'isec{.*}';
-"
+ssh $1 "echo 'Searching for <flags/interesting_file> with content <isec{.*}>';find / -type f -name flags/interesting_file -exec grep 'isec{.*}'  {} \;echo 'Searching for file with content <isec{.*}>';find -type f -exec grep 'isec{.*}'  {} \;echo 'Searching environment of current process';printenv | grep 'isec{.*}';"
 
 fi
